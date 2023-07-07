214755528e26ed99627ccaa9b15cfcfa4500762e
Carl Saginsin <saguinsincarl8@gmail.com>
2023-07-07 11:13:37 +0700

    11:20 am updates
    I created the navbar and the home components.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+// Components
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../assets/logo/logo.png';
+import displayimg from "../assets/assets/streetware 6.jpg"
+import displayimg2 from  "../assets/assets/streetware 3.jpg"
+import displayimg3 from "../assets/assets/streetware 5.jpg"
+
+const Home = () => {
+  return (
+    <>
+    {/* Logo only section */}
+        <section>
+            <img className='Logo' src={Logo} alt="Logo" />
+        </section>
+    {/* end  */}
+
+        {/* first section */}
+        <div className='container'>
+            <img className='image1' src={displayimg} alt='models' />
+            <img className='image2' src={displayimg2} alt='models' />
+            <img className='image3' src={displayimg3} alt='models' />
+        </div>
+        {/* end */}
+    </>
+  );
+};
+
+export default Home;
