900730a0deacdc8f36d522835af71aeb8b5bd24a
Carl Saginsin <saguinsincarl8@gmail.com>
2023-07-07 19:07:12 +0700

    7:10 pm updates 07-07-2023
    I created a footer section.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import { Products } from './Components/Home';
+import { Footer } from './Components/Home';
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Home />
       <Products />
+      <Footer />
     </>
   );
 }
